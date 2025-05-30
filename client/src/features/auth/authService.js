bf8e7703c2aa038458b6b09c0ca6d2f3330b6c0f
Frahyang <frahyang2005@gmail.com>
2025-05-30 15:28:29 +0700

    CICD

--- client/src/features/auth/authService.js
+++ client/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = '/user'
+const API_URL = '/service/user'
 
 // signin user
 const signin = async (userData) => {
--- client/src/pages/ActivateAccount.jsx
+++ client/src/pages/ActivateAccount.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import axios from "axios";
+import axios from "axiosConfig.js";
 import toast from "react-hot-toast";
 
 const ActivateAccount = () => {
