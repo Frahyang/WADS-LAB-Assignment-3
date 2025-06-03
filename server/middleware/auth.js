a7a6b411bdbd4d84611ae0d7665af06bcb1b8b94
Frahyang <frahyang2005@gmail.com>
2025-06-03 10:20:20 +0700

    CICD

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,14 +3,14 @@ import jwt from 'jsonwebtoken';
 export const auth = (req, res, next) => {
     try {
         const token = req.headers.authorization?.split(" ")[1];
-        // console.log(token)
+        console.log(token)
         if (!token) return res.status(403).json({ message: "Token Expired or Invalid Authentication." })
 
         jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
             if (err) return res.status(403).json({ message: "Token Expired or Invalid Authentication." })
 
             req.user = user
-            // console.log(user)
+            console.log(user)
             next()
         })
     } catch (error) {
